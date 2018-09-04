621985c4e0dc98f3176493e5e7c849fd1819ed65
psych0der <mayankbhola@gmail.com>
2018-09-04 16:38:27 +0530

    add commons and helpers

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -3,3 +3,10 @@ export const IDLE = 'IDLE';
 export const IN_PROGRESS = 'IN_PROGRESS';
 export const SUCCESS = 'SUCCESS';
 export const FAILED = 'FAILED';
+export const EASY = 'EASY';
+export const MEDIUM = 'MEDIUM';
+
+export const HARD = 'HARD';
+export const SHORT = 'SHORT';
+export const RESPECTABLE = 'RESPECTABLE';
+export const LENGTHY = 'LENGTHY';
