14e1db07a3fae74df5b6e779b41ca6dfb6d759d7
psych0der <mayankbhola@gmail.com>
2018-09-04 18:54:17 +0530

    remove consoles

--- src/commons/helpers/index.js
+++ src/commons/helpers/index.js
@@ -7,11 +7,9 @@ export const networkErrorHandler = (error: object | Error) => {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    console.error(error.request);
     throw new Error(error.request);
   } else {
     // Something happened in setting up the request that triggered an Error
-    console.error('Error', error.message);
     throw new Error(error.message);
   }
 };
