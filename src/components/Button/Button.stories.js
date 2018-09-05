91f1f1ef0f18cda10c1df2508f17e4c04f0c8f2f
psych0der <mayankbhola@gmail.com>
2018-09-05 19:57:01 +0530

    add small button type

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,6 +7,7 @@ import Button from './index.js';
 storiesOf('Button', module)
   .add('default', () => <Button />)
   .add('custom text', () => <Button text="custom text" />)
+  .add('small ', () => <Button text="small button" small={true} />)
   .add('loading', () => <Button isLoading={true} />)
   .add('disabled', () => <Button disabled={true} />)
   .add('custom color', () => <Button backgroundColor="green" />)
--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Button from './index.js';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 it('renders default button state', () => {
   const component = renderer.create(<Button />);
@@ -12,6 +12,11 @@ it('renders default button state', () => {
 it('renders disabled button correctly', () => {
   const wrapper = mount(<Button disabled={true} />);
   const button = wrapper.find('button');
-  console.log(button.props()['disabled']);
   expect(button.props()['disabled']).toEqual(true);
+});
+
+it('renders small button correctly', () => {
+  const wrapper = shallow(<Button small={true} />);
+  const button = wrapper.find('button');
+  expect(button.props()['className'].split(' ').length).toEqual(2);
 });
