f411fdd37311c2854ec083a6232cbcf53e2edb88
psych0der <mayankbhola@gmail.com>
2018-09-05 22:01:27 +0530

    Add question page component

--- src/components/QuestionPage/index.js
+++ src/components/QuestionPage/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import styles from './index.css';
-import { Button } from '../index';
+import { Button, QuestionBox } from '../index';
 import SweetAlert from 'react-bootstrap-sweetalert';
 import { IconContext } from 'react-icons';
 import { IoMdArrowRoundForward, IoMdArrowRoundBack } from 'react-icons/io';
@@ -9,6 +9,8 @@ type Props = {
   questions: Array<{}>,
   currentQuestion: number,
   abortQuiz: () => *,
+  submittedAnswers: Array<Object>,
+  submitAnswer: () => *,
 };
 type State = {
   questionPointer: number, // this is to keep track of question being displayed
@@ -16,7 +18,7 @@ type State = {
 };
 class QuestionPage extends Component<Props, State> {
   state = {
-    questionPointer: 1,
+    questionPointer: this.props.currentQuestion,
     showConfirm: false,
   };
 
@@ -29,10 +31,63 @@ class QuestionPage extends Component<Props, State> {
     this.setState({ showConfirm: false });
   };
 
+  /* sets current pointer to previous question if allowed */
+  prevQuestion = () => {
+    const questionPointer = this.state.questionPointer;
+    if (questionPointer > 1) {
+      console.log('changing');
+      this.setState({ questionPointer: questionPointer - 1 });
+    }
+  };
+
+  /* sets current pointer to next question if allowed */
+  nextQuestion = () => {
+    const questionPointer = this.state.questionPointer;
+    if (
+      questionPointer < this.props.currentQuestion &&
+      questionPointer <= this.props.questions.length
+    ) {
+      this.setState({ questionPointer: questionPointer + 1 });
+    }
+  };
+
+  /**
+   * Submits answer and iterates current question marker.
+   */
+  submitAnswer = (answer: boolean) => {
+    this.props.submitAnswer(answer);
+    const questionPointer = this.state.questionPointer;
+    if (questionPointer < this.props.questions.length) {
+      this.setState({ questionPointer: questionPointer + 1 });
+    }
+  };
+
   render() {
     return (
       <div className={styles.questionPage}>
-        edit me: at src/components/QuestionPage/index.js
+        <div>
+          <QuestionBox
+            question={this.props.questions[this.state.questionPointer - 1]}
+            answer={
+              this.state.questionPointer < this.props.currentQuestion
+                ? this.props.submittedAnswers[this.state.questionPointer - 1]
+                : null
+            }
+            viewOnly={this.state.questionPointer !== this.props.currentQuestion}
+            submitAnswer={this.submitAnswer}
+            key={`question-box-key-${this.state.questionPointer}`}
+          />
+          <div className={styles.questionNumberIdentifier}>
+            {this.state.questionPointer} of {this.props.questions.length}
+          </div>
+          <div className={styles.previousQuestionWarning}>
+            {this.state.questionPointer !== this.props.currentQuestion && (
+              <span>
+                You can only view the questions you have already answered
+              </span>
+            )}
+          </div>
+        </div>
         <div className={styles.NavigationContainer}>
           <div
             className={
@@ -48,11 +103,13 @@ class QuestionPage extends Component<Props, State> {
           </div>
           <div
             className={
+              this.state.questionPointer < this.props.currentQuestion &&
               this.state.questionPointer <= this.props.questions.length
                 ? styles.right
                 : styles.disabledNav
             }
             onClick={
+              this.state.questionPointer < this.props.currentQuestion &&
               this.state.questionPointer <= this.props.questions.length
                 ? this.nextQuestion
                 : () => {}
@@ -66,6 +123,7 @@ class QuestionPage extends Component<Props, State> {
         <div className={styles.abortButtonContainer}>
           <Button
             backgroundColor="#D41602"
+            small={true}
             text="Abort"
             onClick={this.confirmAbort}
           />
