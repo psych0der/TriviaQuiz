650581d446a3d6d18b42a6e36e4a154520cb4496
psych0der <mayankbhola@gmail.com>
2018-09-05 22:01:40 +0530

    export new components for easy access

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as SquareRadioButton } from './SquareRadioButton';
-export { default as SquareRadioButtonGroup } from './SquareRadioButtonGroup';
+export { default as RoundRadioButton } from './RoundRadioButton';
+export { default as RadioButtonGroup } from './RadioButtonGroup';
+export { default as Button } from './Button';
+export { default as QuestionPage } from './QuestionPage';
+export { default as QuestionBox } from './QuestionBox';
